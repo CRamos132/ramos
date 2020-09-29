043da8ac30252ea7540fe1246014a6073af73a69
HeitorRamos132 <heitorgpramos95@gmail.com>
2020-09-29 17:25:33 -0300

    EV Test

--- src/repositories/GetApi.ts
+++ src/repositories/GetApi.ts
@@ -2,7 +2,7 @@ function GetApi(url: string): string {
   console.log(process.env.NODE_ENV)
   console.log(process.env.GITHUB_TOKEN)
   console.log(process.env.NEXT_PUBLIC_TESTE_EV)
-  return `Dale ${url}`
+  return `Dale, ${url}`
 }
 
 export default GetApi
